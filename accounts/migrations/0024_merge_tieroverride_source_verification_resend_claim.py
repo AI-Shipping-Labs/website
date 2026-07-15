2f3e9c7ed19eda025c08169d1dc5130f85932094
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-07-15 02:22:28 +0200

    Merge accounts migration branches
    Refs #449

---
+++ accounts/migrations/0024_merge_tieroverride_source_verification_resend_claim.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("accounts", "0023_tieroverride_source"),
+        ("accounts", "0023_user_verification_resend_claim"),
+    ]
+
+    operations = []
